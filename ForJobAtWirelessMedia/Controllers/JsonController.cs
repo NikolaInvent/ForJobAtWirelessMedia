8cbee4c9a8b924c9b2a9cf4d13fb5bd8573fc6cd
Nikola Cavic <n.cavic11@gmail.com>
2018-07-20 10:57:59 +0200

    Publishing the site

---
+++ ForJobAtWirelessMedia/Controllers/JsonController.cs
@@ -0,0 +1,63 @@
+﻿using ForJobAtWirelessMedia.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForJobAtWirelessMedia.Controllers
+{
+    public class JsonController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<Product> ProductList = new List<Product>();
+            using (WirelessMediaDBModel db = new WirelessMediaDBModel())
+            {
+                ProductList = db.Products.ToList();
+            }
+            return View(ProductList);
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase jsonFile)
+        {
+            using (WirelessMediaDBModel db = new WirelessMediaDBModel())
+            {
+                if (!jsonFile.FileName.EndsWith(".json"))
+                {
+                    ViewBag.Error = "Invalid file type";
+                }
+                else
+                {
+                    jsonFile.SaveAs(Server.MapPath("~/JSON/" + Path.GetFileName(jsonFile.FileName)));
+                    StreamReader streamReader = new StreamReader(Server.MapPath("~/JSON/" + Path.GetFileName(jsonFile.FileName)));
+                    string data = streamReader.ReadToEnd();
+                    List<Product> products = JsonConvert.DeserializeObject<List<Product>>(data);
+
+                    products.ForEach(p => {
+                        Product product = new Product()
+                        {
+                            Name = p.Name,
+                            Description = p.Description,
+                            Category = p.Category,
+                            Manufacturer = p.Manufacturer,
+                            Supplier = p.Supplier,
+                            Price = p.Price,
+                           
+                        };
+                        db.Products.Add(product);
+                        db.SaveChanges();
+                    });
+                    ViewBag.Success = "Success";
+                }
+            }
+            return View("Index");
+        }
+    }
+}  
+    
---
+++ ForJobAtWirelessMedia/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ForJobAtWirelessMedia.Models;
+using System.Data.Entity;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ForJobAtWirelessMedia.Controllers
+{
+    public class ProductController : Controller
+    {
+         WirelessMediaDBModel db = new WirelessMediaDBModel();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+        [HttpGet]
+       public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int Id = 0)
+        {
+            Product product = db.Products.Find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(product);
+        }
+
+
+    }
+}
